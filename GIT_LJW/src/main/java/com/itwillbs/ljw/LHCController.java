dc19f66cf744c3e3980db3ca6c522df14df48741
jaeoe <101246097+jaeoe@users.noreply.github.com>
2026-02-05 12:34:14 +0900

    Delete GIT_LJW directory

--- GIT_LJW/src/main/java/com/itwillbs/ljw/LHCController.java
+++
@@ -1,6 +0,0 @@
-package com.itwillbs.ljw;
-
-public class LHCController {
-
-	// 내꺼 HC 푸시 테스트. - 12:12 
-}
--- GIT_LJW/src/main/java/com/itwillbs/ljw/LJWController.java
+++
@@ -1,7 +0,0 @@
-package com.itwillbs.ljw;
-
-public class LJWController {
-
-
-	// HC 푸시 테스트. - 12:11 
-}
